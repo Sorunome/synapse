9e132059fea7c693a3ce0d1627f1558392d25b6d
Sorunome <mail@sorunome.de>
2018-03-24 13:31:33 +0100

    change synctl to use python2

--- synapse/app/synctl.py
+++ synapse/app/synctl.py
@@ -74,7 +74,7 @@ def start(configfile):
 
 def start_worker(app, configfile, worker_configfile):
     args = [
-        "python", "-B",
+        "python2", "-B",
         "-m", app,
         "-c", configfile,
         "-c", worker_configfile
